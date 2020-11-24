cf95501b453526b4c7167bf2e0e34870b287d421
Lucas Sales <saless182@gmail.com>
2020-11-24 12:55:56 +0100

    Update build.gradle.kts

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,10 +1,15 @@
 import dk.nstack.kotlin.plugin.TranslationPlugin
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
-apply(plugin = "com.android.application")
-apply(plugin = "kotlin-android")
-apply(plugin = "kotlin-android-extensions")
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
 apply(plugin = "androidx.navigation.safeargs.kotlin")
-apply(plugin = TranslationPlugin)
+
+apply(plugin = TranslationPlugin())
 
 translation {
     appId = "dTyz7mezul3YJliMtsoM3z8ZhmWqDR8kJLbF"
@@ -15,19 +20,19 @@ translation {
 
 android {
     signingConfigs {
-        release {
-            storePassword("test1234")
+        register("release") {
+            storePassword = "test1234"
             keyAlias = "key_test"
-            keyPassword("test1234")
-            storeFile file ("/Users/lucassales/StudioProjects/nstack-kotlin/demo/keystore")
+            keyPassword = "test1234"
+            storeFile = file("/Users/lucassales/StudioProjects/nstack-kotlin/demo/keystore")
         }
     }
     compileSdkVersion(29)
     buildToolsVersion("29.0.3")
 
     compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_1_8)
-        targetCompatibility(JavaVersion.VERSION_1_8)
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
@@ -35,61 +40,54 @@ android {
     }
 
     defaultConfig {
-        applicationId("dk.nodes.nstack.nstackkotlintest")
+        applicationId = "dk.nodes.nstack.nstackkotlintest"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode(1)
-        versionName("1.0")
-        testInstrumentationRunner("android.support.test.runner.AndroidJUnitRunner")
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
 
 
-        manifestPlaceholders = [
-            appId : translation.appId,
-        apiKey: translation.apiKey,
-        ]
+        manifestPlaceholders = mapOf(
+                "appId" to translation.appId,
+                "apiKey" to translation.apiKey
+        )
+
 
     }
     buildTypes {
-        debug {
-            manifestPlaceholders = [
-                env: 'staging'
-            ]
+        named("debug") {
+            manifestPlaceholders.putAll(mapOf("env" to "staging"))
         }
-        release {
-            manifestPlaceholders = [
-                env: 'production'
-            ]
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile ('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        named("release") {
+            manifestPlaceholders.putAll(mapOf("env" to "production"))
+            isMinifyEnabled = false
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
         }
     }
 }
 
 dependencies {
-    implementation(project(mapOf("path" to ":")))
-    implementation(project(mapOf("path" to ":")))
-    implementation fileTree (dir: 'libs', include: ['*.jar'])
+    implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.fragment:fragment-ktx:1.2.5")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version"
-
-    implementation "androidx.appcompat:appcompat:1.2.0"
-    implementation "androidx.core:core-ktx:1.3.2"
-    implementation "androidx.fragment:fragment-ktx:1.2.5"
-    implementation "androidx.lifecycle:lifecycle-extensions:2.2.0"
-    implementation "androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0"
-    implementation "androidx.lifecycle:lifecycle-livedata-ktx:2.2.0"
-    implementation "androidx.lifecycle:lifecycle-runtime-ktx:2.2.0"
-    implementation "androidx.navigation:navigation-fragment-ktx:2.3.1"
-    implementation "androidx.navigation:navigation-ui-ktx:2.3.1"
-    implementation "androidx.constraintlayout:constraintlayout:2.0.4"
-    implementation "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
-
-    implementation 'com.google.android.material:material:1.2.1'
+    implementation("com.google.android.material:material:1.2.1")
 
 
-    implementation project (':nstack-kotlin-android')
+    implementation(project(":nstack-kotlin-android"))
 
-    testImplementation 'junit:junit:4.12'
-    androidTestImplementation 'com.android.support.test:runner:1.0.2'
-    androidTestImplementation 'com.android.support.test.espresso:espresso-core:3.0.2'
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
 }
